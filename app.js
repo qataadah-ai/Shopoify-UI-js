77643fb8effd1e4d3d7a69f720fa621426871631
Qataadah <qaxiafridi@gmail.com>
2026-01-06 21:25:48 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,93 @@
+const ProductsData = [
+
+  {
+    id: 1,
+    Product:
+      '3" Airlift High-Waist Polka Dot Short - Black/white 3" Airlift High-Waist Polka Dot Short - Black/white',
+    Status: "Active",
+    Inventory: "Inventory not ranked",
+    Category: null,
+    Channels: 2,
+    Catalogs: 1,
+    image:
+    "https://cdn.shopify.com/s/files/1/0731/4689/2519/files/Main_52f8e304-92d9-4a36-82af-50df8fe31c69_40x40@3x.jpg?v=1742263839 ",
+  },
+   {
+    id: 2,
+    Product:
+      '3" Airlift High-Waist Polka Dot Short - Black/white 3" Airlift High-Waist Polka Dot Short - Black/white',
+    Status: "Archived",
+    Inventory: "Inventory not ranked",
+    Category: null,
+    Channels: 2,
+    Catalogs: 1,
+    image:
+      "https://cdn.shopify.com/s/files/1/0731/4689/2519/files/W3438R_01_b1_s1_a1_1_m77_cfe57ce7-97e1-415b-8c8d-c39ba9c9e038_40x40@3x.jpg?v=1742382850",
+  },
+   {
+    id: 3,
+    Product:"Qazi Qataadah",
+     Status: "Draft",
+    Inventory: "Inventory not ranked",
+    Category: null,
+    Channels: 2,
+    Catalogs: 1,
+    image:
+      "https://cdn.shopify.com/s/files/1/0731/4689/2519/files/W3438R_01_b1_s1_a1_1_m77_cfe57ce7-97e1-415b-8c8d-c39ba9c9e038_40x40@3x.jpg?v=1742382850",
+  },
+
+   {
+    id: 4,
+    Product:"Afrdi",
+    Status: "Active",
+    Inventory: "Inventory not ranked",
+    Category: null,
+    Channels: 2,
+    Catalogs: 1,
+    image:
+      "https://cdn.shopify.com/s/files/1/0731/4689/2519/files/W3438R_01_b1_s1_a1_1_m77_cfe57ce7-97e1-415b-8c8d-c39ba9c9e038_40x40@3x.jpg?v=1742382850",
+  },
+
+   {
+    id: 5,
+    Product:
+      '3" Airlift High-Waist Polka Dot Short - Black/white 3" Airlift High-Waist Polka Dot Short - Black/white',
+    Status: "Active",
+    Inventory: "Inventory not ranked",
+    Category: null,
+    Channels: 2,
+    Catalogs: 1,
+    image:
+      "https://cdn.shopify.com/s/files/1/0731/4689/2519/files/W3438R_01_b1_s1_a1_1_m77_cfe57ce7-97e1-415b-8c8d-c39ba9c9e038_40x40@3x.jpg?v=1742382850",
+  },
+
+
+   {
+    id: 6,
+    Product:
+      '3" Airlift High-Waist Polka Dot Short - Black/white 3" Airlift High-Waist Polka Dot Short - Black/white',
+    Status: "Draft",
+    Inventory: "Inventory not ranked",
+    Category: null,
+    Channels: 2,
+    Catalogs: 1,
+    image:
+      "https://cdn.shopify.com/s/files/1/0731/4689/2519/files/W3438R_02910_b1_s1_a1_1_m54_40x40@3x.jpg?v=1742382789",
+  },
+
+
+   {
+    id: 7,
+    Product:
+      '3" Airlift High-Waist Polka Dot Short - Black/white 3" Airlift High-Waist Polka Dot Short - Black/white',
+    Status: "Active",
+    Inventory: "Inventory not ranked",
+    Category: null,
+    Channels: 2,
+    Catalogs: 1,
+    image:
+      "https://cdn.shopify.com/s/files/1/0731/4689/2519/files/W3438R_02910_b1_s1_a1_1_m54_40x40@3x.jpg?v=1742382789",
+  },
+];
+
+

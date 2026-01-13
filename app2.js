020c6549d1208bd5cc2d48e57ca483ae1ef28672
Qataadah <qaxiafridi@gmail.com>
2026-01-13 20:09:12 +0500

    Updated

--- app2.js
+++ app2.js
@@ -35,7 +35,8 @@ document.querySelectorAll("ul[id]:not(#main-list) li:not(#back-to-main)").forEac
   };
 });
 
-// Price Dropdown
+
+// Price Dropdown 
 
 const priceBtn = document.getElementById("Price-btn-dropdown-icon");
 const priceDropdown = document.getElementById("price-dropdown");
@@ -48,6 +49,7 @@ priceBtn.onclick=()=>{
 }
 
 // Unit Price Dropdown
+
 const unitInput = document.getElementById("unit-price");
 const unitDropdown = document.getElementById("unit-price-dropdown");
 const CancerBtn = document.getElementById("unit-price-cancel-btn");
@@ -62,9 +64,14 @@ CancerBtn.onclick = ( )=>{
 
 const CompareBtn = document.getElementById("Compare-at-btn")
 
+const compareInput = document.getElementById("Compare-at");
+
 CompareBtn.onclick = () =>{
    priceDropdown.classList.remove("hidden");
     priceButtons.classList.add("hidden");
+    priceBtn.style.rotate = "180deg";
+
+    compareInput.focus();
 }
 
 const totalAmountInput = document.querySelector('#toal-amount');
@@ -75,17 +82,75 @@ UnitPriceBtn.onclick = () => {
     priceDropdown.classList.remove("hidden");
     priceButtons.classList.add("hidden");
     unitDropdown.classList.remove("hidden"); 
-  
+      priceBtn.style.rotate = "180deg";
     totalAmountInput.focus();
 };
 
 
-const inventoryBtn = document.getElementById("Inventory-dropdrown-btn");
+
+// Inventory Dropdown 
+
+const inventoryDropBtn = document.getElementById("Inventory-dropdrown-btn");
 const inventoryDropdown = document.getElementById("Inventory-dropdrown");
 const inventoryButtons = document.getElementById("Inventory-btns");
 
-inventoryBtn.onclick = () => {
-  inventoryBtn.style.rotate = inventoryDropdown.classList.contains("hidden") ? "180deg" : "0deg";
+
+const SKUInput = document.getElementById("SKU");
+const barcodeInput = document.getElementById("Barcode");
+
+const skuBtn = document.getElementById("SKU-btn").onclick = () => {
+
+ inventoryDropBtn.style.rotate = "180deg";
+
+inventoryDropdown.classList.remove("hidden");
+
+inventoryButtons.classList.add("hidden");
+SKUInput.focus();
+}
+
+const barcodeBtn = document.getElementById("barcode-btn").onclick = () => {
+inventoryDropdown.classList.remove("hidden");
+ inventoryDropBtn.style.rotate = "180deg";
+inventoryButtons.classList.add("hidden");
+barcodeInput.focus();
+}
+
+inventoryDropBtn.onclick = () => {
+  inventoryDropBtn.style.rotate = inventoryDropdown.classList.contains("hidden") ? "180deg" : "0deg";
   inventoryDropdown.classList.toggle("hidden");
   inventoryButtons.classList.toggle("hidden");
+}
+// Shipping dropdown
+
+const ShippingDropBtn = document.getElementById("Shipping-dropdrown-btn");
+const ShippingDropdown = document.getElementById("Shipping-dropdrown");
+const ShippingButtons = document.getElementById("Shipping-btns");
+
+
+ShippingDropBtn.onclick = () => {
+ShippingDropdown.classList.toggle("hidden");
+ShippingDropBtn.style.rotate = ShippingDropdown.classList.contains("hidden") ? "0deg" : "180deg";
+ShippingButtons.classList.toggle("hidden");
+}
+
+
+const CountryInput = document.getElementById("Country-region");
+const HSInput = document.getElementById("HS-code");
+
+
+ document.getElementById("country-btn").onclick = () => {
+
+ ShippingDropBtn.style.rotate = "180deg";
+
+ShippingDropdown.classList.remove("hidden");
+
+ShippingButtons.classList.add("hidden");
+CountryInput.focus();
+}
+
+ document.getElementById("HS-btn").onclick = () => {
+ShippingDropdown.classList.remove("hidden");
+ ShippingDropBtn.style.rotate = "180deg";
+ShippingButtons.classList.add("hidden");
+HSInput.focus();
 }
